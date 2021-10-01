ccede0be7053028eb2b203f9c195d03aa153dd12
Rohhian <kristianmiilen@gmail.com>
2021-10-01 19:37:52 +0300

    added Array

--- Main.java
+++ Main.java
@@ -4,13 +4,16 @@ public class Main {
     
     public static void main(String[] args) {
         Random randomNum = new Random();
-        int firstNumber = randomNum.nextInt(2);
+        int firstNumber = randomNum.nextInt(5);
         System.out.println("the random number is " + firstNumber);
-        if (firstNumber == 1) {
+        if (firstNumber != 0) {
             System.out.println("hooray something has emerged!");
         } else {
                 System.out.println("there is only void...");
             }
+        
+        int[] array = {11,12,13,14,15};
+        System.out.println(array[firstNumber]);
 
         }
     }
